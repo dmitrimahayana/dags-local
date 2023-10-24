b02c37515bfd448684bce8a0812d6cdfd5a1e141
dmitrimahayana <dmitri.mahayana@gmail.com>
2023-10-24 09:11:15 +0700

    Update GCP_Digimon_ETL.py

--- GCP_Digimon_ETL.py
+++ GCP_Digimon_ETL.py
@@ -14,7 +14,7 @@ from airflow.contrib.operators.bigquery_operator import BigQueryOperator
 default_args = {
     'owner': 'Dmitri',
     'start_date': datetime(2023, 9, 12),
-    'retries': 1,  # Number of retries if a task fails
+    'retries': 0,  # Number of retries if a task fails
     'retry_delay': timedelta(minutes=5),  # Time between retries
 }
 
